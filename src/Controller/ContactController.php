af38583cb69857dc09c74a0574a987a3b8556be7
jimmy-martin <jimmy.martin952@gmail.com>
2021-11-06 13:21:43 +0100

    add constraints in contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Form\ContactType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,11 +17,17 @@ class ContactController extends AbstractController
     /**
      * @Route("", name="browse")
      */
-    public function browse(UserRepository $userRepository): Response
+    public function browse(UserRepository $userRepository, Request $request): Response
     {
         $me = $userRepository->find(1);
 
         $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd($form->getData());
+        }
 
         return $this->render('contact/browse.html.twig', [
             'form' => $form->createView(),
