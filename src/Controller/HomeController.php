3ecbf422f01d778229327b0ae7f8c22cc1b2141a
jimmy-martin <jimmy.martin952@gmail.com>
2021-11-07 00:43:28 +0100

    caching datas for faster loadings

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Repository\ProjectRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class HomeController extends AbstractController
 {
@@ -15,13 +17,28 @@ class HomeController extends AbstractController
      */
     public function home(UserRepository $userRepository, ProjectRepository $projectRepository): Response
     {
-        $me = $userRepository->find(1);
+        $cache = new FilesystemAdapter();
+
+        $me = $cache->get('my_profil', function(ItemInterface $item) use ($userRepository) {
+            $item->expiresAfter(600);
+
+            return $userRepository->find(1);
+        });
+
+        // $me = $userRepository->find(1);
 
         $myEmail = $this->getParameter('app.my_email');
 
-        $projects = $projectRepository->findLastSix();
 
-        dump($me, $projects);
+        $projects = $cache->get('last_six_projects', function(ItemInterface $item) use ($projectRepository) {
+            $item->expiresAfter(600);
+
+            return $projectRepository->findLastSix();
+        });
+
+        // $projects = $projectRepository->findLastSix();
+
+        // dump($me, $projects);
 
         return $this->render('home/home.html.twig', [
             'me' => $me,
--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\ItemInterface;
 
 /**
  * @Route("/portfolio", name="portfolio_")
@@ -17,7 +19,15 @@ class PortfolioController extends AbstractController
      */
     public function browse(ProjectRepository $projectRepository): Response
     {
-        $projects = $projectRepository->findAll();
+        $cache = new FilesystemAdapter();
+
+        $projects = $cache->get('all_projects', function(ItemInterface $item) use ($projectRepository) {
+            $item->expiresAfter(600);
+
+            return $projectRepository->findAll();
+        });
+
+        // $projects = $projectRepository->findAll();
 
         return $this->render('portfolio/browse.html.twig', [
             'projects' => $projects,
