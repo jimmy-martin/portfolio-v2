a8b3f2f08b45732574d30fe55ab89996cb595910
jimmy-martin <jimmy.martin952@gmail.com>
2021-11-06 23:04:07 +0100

    send email done

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,7 +7,11 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+
+use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
 /**
  * @Route("/contact", name="contact_")
@@ -17,7 +21,7 @@ class ContactController extends AbstractController
     /**
      * @Route("", name="browse")
      */
-    public function browse(UserRepository $userRepository, Request $request): Response
+    public function browse(UserRepository $userRepository, Request $request, MailerInterface $mailer): Response
     {
         $me = $userRepository->find(1);
 
@@ -26,7 +30,21 @@ class ContactController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            dd($form->getData());
+            // dd($form->getData());
+
+            $datas = $form->getData();
+
+            $myEmail = $this->getParameter('app.my_email');
+
+            $email = (new Email())
+                ->from($datas['email'])
+                ->to($myEmail)
+                ->subject($datas['subject'])
+                ->text('De la part de ' . $datas['name'] . ' : ' . $datas['message']);
+
+            $mailer->send($email);
+
+            return $this->redirectToRoute('home');
         }
 
         return $this->render('contact/browse.html.twig', [
