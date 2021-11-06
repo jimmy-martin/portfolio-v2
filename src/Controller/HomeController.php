d87415f88fb223f9ca323cc88547373eb1d52ba2
jimmy-martin <jimmy.martin952@gmail.com>
2021-11-06 11:34:05 +0100

    displays last six projects in portfolio previews

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,14 +13,17 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home(UserRepository $userRepository): Response
+    public function home(UserRepository $userRepository, ProjectRepository $projectRepository): Response
     {
         $me = $userRepository->find(1);
 
-        dump($me);
+        $projects = $projectRepository->findLastSix();
+
+        dump($me, $projects);
 
         return $this->render('home/home.html.twig', [
             'me' => $me,
+            'projects' => $projects,
         ]);
     }
 }
