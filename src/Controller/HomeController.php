cde32f655010c99804b5e09902535294cc325944
jimmy-martin <jimmy.martin952@gmail.com>
2021-11-06 11:13:49 +0100

    home 'A propos' done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,14 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function home(UserRepository $userRepository): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        $me = $userRepository->find(1);
+
+        dump($me);
+
+        return $this->render('home/home.html.twig', [
+            'me' => $me,
         ]);
     }
 }
