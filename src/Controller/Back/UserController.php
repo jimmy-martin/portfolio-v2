a4189b90755854d0b4b513c6ba85bd5f0e4df928
jimmy-martin <jimmy.martin952@gmail.com>
2021-11-11 20:38:08 +0100

    remove unused use

--- src/Controller/Back/UserController.php
+++ src/Controller/Back/UserController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
 /**
  * @Route("/admin/user", name="back_user_")
