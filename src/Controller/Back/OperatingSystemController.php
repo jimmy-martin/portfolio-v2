ecde6fd054e406f85b802f17b9c99505aab04e82
jimmy-martin <jimmy.martin952@gmail.com>
2021-11-13 11:01:55 +0100

    create BREAD for all backoffice entities

--- src/Controller/Back/OperatingSystemController.php
+++ src/Controller/Back/OperatingSystemController.php
@@ -2,19 +2,102 @@
 
 namespace App\Controller\Back;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\OperatingSystem;
+use App\Form\OperatingSystemType;
+use App\Repository\OperatingSystemRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/admin/operating_system", name="back_operating_system_", requirements={"id"="\d+"})
+ */
 class OperatingSystemController extends AbstractController
 {
-    /**
-     * @Route("/back/operating/system", name="back_operating_system")
-     */
-    public function index(): Response
+    private $manager;
+
+    public function __construct(EntityManagerInterface $manager)
     {
-        return $this->render('back/operating_system/index.html.twig', [
-            'controller_name' => 'OperatingSystemController',
+        $this->manager = $manager;
+    }
+
+    /**
+     * @Route("", name="browse")
+     */
+    public function browse(OperatingSystemRepository $operatingSystemRepository): Response
+    {
+        return $this->render('back/operating_system/browse.html.twig', [
+            'operatingSystems' => $operatingSystemRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function add(Request $request)
+    {
+        $operatingSystem = new OperatingSystem();
+
+        $form = $this->createForm(OperatingSystemType::class, $operatingSystem);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->persist($operatingSystem);
+            $this->manager->flush();
+
+            $this->addFlash('success', 'Le système d\'exploitation a bien été ajouté');
+
+            return $this->redirectToRoute('back_operating_system_browse');
+        }
+
+        return $this->render('back/operating_system/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     */
+    public function edit(Request $request, OperatingSystem $operatingSystem)
+    {
+        $form = $this->createForm(OperatingSystemType::class, $operatingSystem);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->flush();
+
+            $this->addFlash('success', 'Le système d\'exploitation a bien été modifié');
+
+            return $this->redirectToRoute('back_operating_system_browse');
+        }
+
+        return $this->render('back/operating_system/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, OperatingSystem $operatingSystem)
+    {
+        $submittedAntiCSRFToken = $request->request->get('_token');
+
+        if($this->isCsrfTokenValid('delete_operating_system' . $operatingSystem->getId(), $submittedAntiCSRFToken)) {
+            $this->manager->remove($operatingSystem);
+            $this->manager->flush();
+
+            $this->addFlash('success', 'Le système d\'exploitation a bien été supprimé');
+
+            return $this->redirectToRoute('back_operating_system_browse');
+        }
+
+        $this->addFlash('danger', 'Une erreur est survenue lors de la suppression');
+
+        return $this->redirectToRoute('back_operating_system_browse');
     }
 }
--- src/Controller/Back/SoftwareController.php
+++ src/Controller/Back/SoftwareController.php
@@ -2,19 +2,102 @@
 
 namespace App\Controller\Back;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Software;
+use App\Form\SoftwareType;
+use App\Repository\SoftwareRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/admin/software", name="back_software_", requirements={"id"="\d+"})
+ */
 class SoftwareController extends AbstractController
 {
-    /**
-     * @Route("/back/software", name="back_software")
-     */
-    public function index(): Response
+    private $manager;
+
+    public function __construct(EntityManagerInterface $manager)
     {
-        return $this->render('back/software/index.html.twig', [
-            'controller_name' => 'SoftwareController',
+        $this->manager = $manager;
+    }
+
+    /**
+     * @Route("", name="browse")
+     */
+    public function browse(SoftwareRepository $softwareRepository): Response
+    {
+        return $this->render('back/software/browse.html.twig', [
+            'softwares' => $softwareRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function add(Request $request)
+    {
+        $software = new Software();
+
+        $form = $this->createForm(SoftwareType::class, $software);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->persist($software);
+            $this->manager->flush();
+
+            $this->addFlash('success', 'Le logiciel a bien été ajouté');
+
+            return $this->redirectToRoute('back_software_browse');
+        }
+
+        return $this->render('back/software/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     */
+    public function edit(Request $request, Software $software)
+    {
+        $form = $this->createForm(SoftwareType::class, $software);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->flush();
+
+            $this->addFlash('success', 'Le logiciel a bien été modifié');
+
+            return $this->redirectToRoute('back_software_browse');
+        }
+
+        return $this->render('back/software/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Software $software)
+    {
+        $submittedAntiCSRFToken = $request->request->get('_token');
+
+        if($this->isCsrfTokenValid('delete_software' . $software->getId(), $submittedAntiCSRFToken)) {
+            $this->manager->remove($software);
+            $this->manager->flush();
+
+            $this->addFlash('success', 'Le logiciel a bien été supprimé');
+
+            return $this->redirectToRoute('back_software_browse');
+        }
+
+        $this->addFlash('danger', 'Une erreur est survenue lors de la suppression');
+
+        return $this->redirectToRoute('back_software_browse');
     }
 }
