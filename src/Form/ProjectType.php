74369b3267f03f8d4e3b1867bf525a43c31d95c4
jimmy-martin <jimmy.martin952@gmail.com>
2021-11-25 23:06:44 +0100

    allow to dont upload image every time i want to edit a project

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -3,11 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Project;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormEvent;
 
 class ProjectType extends AbstractType
 {
@@ -57,7 +59,31 @@ class ProjectType extends AbstractType
                 'label' => 'Catégories',
                 'multiple' => true,
                 'expanded' => true,
-            ]);
+            ])
+
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                $project = $event->getData();
+                $form = $event->getForm();
+
+                if ($project->getId() !== null) {
+                    $form->remove('image');
+
+                    $form->add('image', FileType::class, [
+                        'label' => 'Nom du fichier image',
+                        'mapped' => false,
+                        'required' => false,
+                        'constraints' => [
+                            new File([
+                                'mimeTypes' => [
+                                    'image/jpeg',
+                                    'image/png',
+                                ],
+                                'mimeTypesMessage' => 'Veuillez envoyer une image au format jpg/jpeg ou png.',
+                            ])
+                        ]
+                    ]);
+                }
+            });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
