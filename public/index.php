c386dfd65b02d5b48de1a4c2b6fc4fd914ddaf22
jimmy-martin <jimmy.martin952@gmail.com>
2021-11-29 22:23:53 +0100

    fix undefined index error

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
 $trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
-if ($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+if (isset($_SERVER['APP_ENV']) && $_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
 if ($trustedProxies) {
     Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_PROTO);
 }
